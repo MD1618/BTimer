d22422a1916be35a7f6c19ff0366646dc521572f
Martin Drahony <1636326@dundeeandangus.ac.uk>
2019-05-19 16:45:56 +0100

    created sessions model

--- Hof_Timer/app/HofSession.php
+++ Hof_Timer/app/HofSession.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class HofSession extends Model
 {
-    //
+    // table name
+    protected $table = 'hof_sessions';
+
+    // primary key
+    public $primaryKey = 'id';
+    
+    // user name
+    public $user_name = 'user';
+
+    // session duration
+    public $session_hour = 0;
+    public $session_min = 0;
+    public $session_sec = 0;
+    
+    // longest hold
+    public $hold_min = 0;
+    public $hold_sec = 0;
+
+    // average hold
+    public $AVhold_min = 0;
+    public $AVhold_sec = 0;
+
+
+    // TimeStamps
+    public $timestamps = true;
 }
