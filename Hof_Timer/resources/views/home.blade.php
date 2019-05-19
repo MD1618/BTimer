e95d483bfaa2bb01934af976b6c6527f9b82d48c
Martin Drahony <1636326@dundeeandangus.ac.uk>
2019-05-19 11:06:12 +0100

    tablel and sample data added to dashboard page

--- Hof_Timer/resources/views/home.blade.php
+++ Hof_Timer/resources/views/home.blade.php
@@ -5,17 +5,38 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Dashboard
+                    <a class="" style="position:absolute;right:30px;" href="{{ url('/') }}">
+                        Back to Timer
+                    </a>
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
-                    Breathing Sessions 
+                    Breathing Sessions
                 </div>
+                <table class="dashboardTable">
+                    <thead>
+                        <tr>
+                            <th>Session Date</th>
+                            <th>Session Duration</th>
+                            <th>Longest Hold</th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>12/05/2019 15:35:12</td>
+                            <td>45mins 12seconds</td>
+                            <td>2mins 23seconds</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
