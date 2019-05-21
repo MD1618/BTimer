6c506df60c355ffffacfa5a2c8406790519c4579
Martin Drahony <1636326@dundeeandangus.ac.uk>
2019-05-21 13:26:52 +0100

    session data query working

--- Hof_Timer/app/Http/Controllers/HomeController.php
+++ Hof_Timer/app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -25,11 +26,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        //return HofSession::all();
+        //return HofSession::all(); //using model
         
-        $hof_sessions = DB::table('hof_sessions')->get();
-        
-        //$hof_sessions = HofSession::all();
+        //get session for current user - needs <use Illuminate\Support\Facades\Auth;>
+        $hof_sessions = DB::table('hof_sessions')->where('user', Auth::id())->orderBy('created_at', 'desc')->get();
+       
         return view('/home')->with('hof_sessions', $hof_sessions);
     }
 
