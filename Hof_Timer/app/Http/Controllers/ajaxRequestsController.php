599807d59f133013a81f0308864d52648a75cccf
Martin Drahony <1636326@dundeeandangus.ac.uk>
2019-05-21 13:27:06 +0100

    ajax save working

--- Hof_Timer/app/Http/Controllers/ajaxRequestsController.php
+++ Hof_Timer/app/Http/Controllers/ajaxRequestsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\HofSession;
+use Illuminate\Support\Facades\Auth;
 
 class ajaxRequestsController extends Controller
 {
@@ -13,16 +15,23 @@ class ajaxRequestsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function ajaxTest(Request $request)
+    public function ajaxSaveTimes(Request $request)
     {
-        $data = $request->all(); // This will get all the request data
-        $dataMin = $request->input('min'); 
-        $datasec = $request->input('sec'); 
-        $dataThour = $request->input('THour'); 
-        $dataTMin = $request->input('TMin'); 
-        $dataTSec = $request->input('TSec') ; 
-        $dataString = $dataMin . ":" . $datasec . " Awesome hold!";
-        $dataTString = $dataThour . ":" . $dataTMin . ":" . $dataTSec . " Nice Session";
-        return response($dataTString  ,200);
+        // $data = $request->all(); // This will get all the request data
+
+        $hofSession = new HofSession();
+        if (Auth::check()) { 
+            $hofSession->user = Auth::id();
+        }
+        $hofSession->session_hour = $request->input('THour');
+        $hofSession->session_min = $request->input('TMin');
+        $hofSession->session_sec = $request->input('TSec');
+        $hofSession->hold_min = $request->input('HMin');
+        $hofSession->hold_sec = $request->input('HSec');
+        $hofSession->AVhold_min = $request->input('AVHMin');
+        $hofSession->AVhold_sec = $request->input('AVHSec');
+
+        $hofSession->save();
+        return response($hofSession, 200);
     }
 }
